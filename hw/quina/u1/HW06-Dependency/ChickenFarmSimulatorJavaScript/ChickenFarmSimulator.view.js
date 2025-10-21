279038211f3d202b3a0e444cde5a5849d6503754
Maryuri3 <maryurisalome3@gmail.com>
2025-10-21 00:19:02 -0500

    v 0.1.0

---
+++ hw/quina/u1/HW06-Dependency/ChickenFarmSimulatorJavaScript/ChickenFarmSimulator.view.js
@@ -0,0 +1,7 @@
+const Chicken = require('./Chicken');
+
+console.log("\nThis is my Chicken Farm Simulator by Maryuri");
+console.log("The Chicken is\n");
+
+const chicken = new Chicken(1, "Lucy", "White and Brown", false, 2);
+console.log(chicken.toString());
