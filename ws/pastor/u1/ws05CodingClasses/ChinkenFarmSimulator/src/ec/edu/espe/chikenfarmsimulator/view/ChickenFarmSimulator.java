1f9c5b5f6e6b65943449494d227917809ad7a60b
mthws-hub <mspastor.h@gmail.com>
2025-10-21 17:49:02 -0500

    v 1.0

---
+++ ws/pastor/u1/ws05CodingClasses/ChinkenFarmSimulator/src/ec/edu/espe/chikenfarmsimulator/view/ChickenFarmSimulator.java
@@ -0,0 +1,36 @@
+package ec.edu.espe.chikenfarmsimulator.view;
+
+import ec.edu.espe.chikenfarmsimulator.model.Chiken;
+
+/**
+ *
+ * @author Mathews Pastor
+ */
+public class ChickenFarmSimulator {
+    public static void main(String arg[]){
+        
+        System.out.println("This is my Chicen Farm Simulator by Mathews Pastor");
+        
+        Chiken chiken2 = new Chiken(0, "Maruja", "white", 1, true);
+        
+        String owner;
+        int id = 1;
+        String name = "Lucy";
+        String color = "White and Brown";
+        int age = 2;
+        boolean isMolting = false;
+        
+        owner = "Mathews Pastor";
+        
+        //Class object
+        Chiken chiken;
+        
+        chiken = new Chiken(id,name,color, age, isMolting);
+        
+        System.out.println("The chiken is --> " + chiken);
+        
+        System.out.println("chicken id --> \t" + chiken.getId() + "-" + chiken.getName());
+        chiken.doStuff();
+        chiken2.doStuff();
+    }
+}
