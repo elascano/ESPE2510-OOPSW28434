938155cae206efbab8c925fea740310cb39b9789
kevanthoc249 <kachalan@espe.edu.ec>
2025-10-21 13:29:54 -0500

    V0.1.2

---
+++ ws/chalan/u1/ws04Dependences/ChikenFarmSimulator/src/utils/FileManager.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package utils;
+
+/**
+ *
+ * @author Kevin Chalan, Object Masters, @ESPE
+ */
+public class FileManager {
+
+    public void save(String fileName, String data) {
+
+    }
+
+    public String read(String fileName) {
+        return "";
+    }
+
+//       public String find(String fileName, String criterium, String data){
+//           return new String();
+//           
+//       }
+    public Object find(String fileName, String criterium, String data) {
+        return new Object();
+    }
+
+    public void update(String fileName, String criterium, String data, String newData) {
+
+    }
+
+    public void delete(String fileName, String criterium, String data) {
+
+    }
+
+}
