419765048d86a65b35433026fe8d83c5392a3048
Fernando Daniel Chicaiza Sambachi <xxfernan626xx@gmail.com>
2025-10-21 18:51:27 -0500

    v0.4

---
+++ ws/chicaiza/u1/ws04_Dependencies/JavaApplication7/src/ec/espe/edu/chickenfarmsimulator/model/Poop.java
@@ -0,0 +1,34 @@
+
+package ec.espe.edu.chickenfarmsimulator.model;
+
+
+public class Poop {
+    private int amount;
+
+    
+    public Poop(int amount) {
+        this.amount = amount;
+    }
+
+    @Override
+    public String toString() {
+    return "Poop{" + "amount=" + amount + '}';
+    }
+    
+    /**
+     * @return the amount
+     */
+    public int getAmount() {
+        return amount;
+    }
+
+    /**
+     * @param amount the amount to set
+     */
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+    
+   
+    
+}
