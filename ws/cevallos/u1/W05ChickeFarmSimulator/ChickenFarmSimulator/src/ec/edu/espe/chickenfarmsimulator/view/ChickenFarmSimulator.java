40dbd1a6276121d08e8d13b985c59e8bb47e4048
Mateo David Cevallos Paguay <cevallosmateo07@gmail.com>
2025-10-21 17:52:39 -0500

    v0.2.0

---
+++ ws/cevallos/u1/W05ChickeFarmSimulator/ChickenFarmSimulator/src/ec/edu/espe/chickenfarmsimulator/view/ChickenFarmSimulator.java
@@ -0,0 +1,39 @@
+package ec.edu.espe.chickenfarmsimulator.view;
+
+import ec.edu.espe.chickenfarmsimulator.model.Chicken;
+
+
+/**
+ *
+ * @author Mateo Cevallos, Object Masters, @ESPE
+ */
+public class ChickenFarmSimulator {
+    public static void main(String args[]){
+        
+        System.out.println("This is my Chicken Farm Simulator");
+        
+        Chicken chicken2 = new Chicken(0, "Maruja", "White" , 1, true);
+        
+        String owner;
+        int id = 1;
+        String name = "Lucy";
+        String color = "White an Bronw";
+        int age = 2;
+        boolean isMolting = false;
+        
+        owner = "Mateo Cevallos";
+        
+        //Class Object
+        Chicken chicken;
+        
+        chicken = new Chicken(id, name, color, age, isMolting);
+        
+        System.out.println("The chicken is" + chicken);
+        
+        System.out.println("chicken id -->"+ chicken.getId() + chicken.getName());
+        
+        chicken.doStuff();
+        
+        
+    }
+}
