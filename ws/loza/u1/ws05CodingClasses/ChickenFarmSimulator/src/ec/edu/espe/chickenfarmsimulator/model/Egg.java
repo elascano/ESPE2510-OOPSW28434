51db74d33452101d83921e383a972e2c2c23e928
Steven <ssloza1@espe.edu.ec>
2025-10-21 19:34:17 -0500

    v1.1.4

---
+++ ws/loza/u1/ws05CodingClasses/ChickenFarmSimulator/src/ec/edu/espe/chickenfarmsimulator/model/Egg.java
@@ -0,0 +1,36 @@
+package ec.edu.espe.chickenfarmsimulator.model;
+
+/**
+ *
+ * @author Steven Loza @ESPE
+ */
+public class Egg {
+    private char size;
+
+    @Override
+    public String toString() {
+        return "Egg{" + "size=" + size + '}';
+    }
+    
+    public Egg(char size) {
+        this.size = size;
+    }
+
+    /**
+     * @return the size
+     */
+    public char getSize() {
+        return size;
+    }
+
+    /**
+     * @param size the size to set
+     */
+    public void setSize(char size) {
+        this.size = size;
+    }
+    
+    
+    
+    
+}
