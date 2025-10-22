721f423cada3ddfe869efe199cefa3d9d39c4fd1
PauloRamos24 <paulo.alramos24@gmail.com>
2025-10-22 00:16:17 -0500

    v0.1.0

---
+++ hw/ramos/u1/hw07ChickenFarmSimulator/Python/Poop.py
@@ -0,0 +1,14 @@
+class Poop:
+    def __init__(self, amount: int):
+        self.__amount = amount  # atributo privado
+
+    def __str__(self):
+        return f"Poop{{amount={self.__amount}}}"
+
+    # Getter
+    def get_amount(self) -> int:
+        return self.__amount
+
+    # Setter
+    def set_amount(self, amount: int):
+        self.__amount = amount
