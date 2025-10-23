053ac7afe26b061635ff41b9faa5aa1e9483f7f9
Fernando Daniel Chicaiza Sambachi <xxfernan626xx@gmail.com>
2025-10-22 21:25:49 -0500

    v0.5

---
+++ hw/chicaiza/u1/hw9AntSimulator/AntSimulator/src/model/FoodPile.java
@@ -0,0 +1,15 @@
+package model;
+
+public class FoodPile {
+    private int amount;
+
+    public FoodPile(int amount) {
+        this.amount = amount;
+        // Set the initial amount of food in the pile
+    }
+
+    // Returns a new portion of food taken from the pile
+    public Food takeFood(int mg) {
+        return null;
+    }
+}
