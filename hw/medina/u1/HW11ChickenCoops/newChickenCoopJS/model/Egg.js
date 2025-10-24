e9f14d72ed9af0870068ff0ac6b10b5dba4bbc8b
Joseph-Benjamin-Medina-Sanchez <joseph3elcrac@gmail.com>
2025-10-23 20:29:50 -0500

    v 0.2.0

---
+++ hw/medina/u1/HW11ChickenCoops/newChickenCoopJS/model/Egg.js
@@ -0,0 +1,22 @@
+/**
+ * Egg class
+ * Represents an egg laid by a chicken.
+ */
+class Egg {
+  /**
+   * @param {string} size
+   */
+  constructor(size) {
+    this.size = size;
+  }
+
+  toString() {
+    return `Egg{ size = ${this.size} }`;
+  }
+
+  getSize() {
+    return this.size;
+  }
+}
+
+module.exports = { Egg };
