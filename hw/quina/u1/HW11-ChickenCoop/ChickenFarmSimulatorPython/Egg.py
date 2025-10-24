390ff4119e520e234ef7a9faa089f1710ac962fd
Maryuri3 <maryurisalome3@gmail.com>
2025-10-23 23:16:15 -0500

    v 0.2.0

---
+++ hw/quina/u1/HW11-ChickenCoop/ChickenFarmSimulatorPython/Egg.py
@@ -0,0 +1,11 @@
+class Egg:
+    def __init__(self, size):
+        self.size = size
+
+    def __str__(self):
+        return f"a {self.size} size egg"
+
+
+def lay_egg(chicken, size):
+    egg = Egg(size)
+    print(f"chicken {chicken.name} is laying {egg}")
