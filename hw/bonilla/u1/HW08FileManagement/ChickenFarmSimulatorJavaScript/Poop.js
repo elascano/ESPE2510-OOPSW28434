0d7f45b915c708913e83336e75bb3d82ef0d2c64
Samiabc <asbonilla7@espe.edu.ec>
2025-10-26 20:26:11 -0500

    v 0.1.0

---
+++ hw/bonilla/u1/HW08FileManagement/ChickenFarmSimulatorJavaScript/Poop.js
@@ -0,0 +1,19 @@
+class Poop {
+  constructor(amount) {
+    this.amount = amount;
+  }
+
+  toString() {
+    return `Poop{amount=${this.amount}}`;
+  }
+
+  getAmount() {
+    return this.amount;
+  }
+
+  setAmount(amount) {
+    this.amount = amount;
+  }
+}
+
+module.exports = { Poop };
