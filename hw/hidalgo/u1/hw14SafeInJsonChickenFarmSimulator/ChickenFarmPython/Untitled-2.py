3f119a3fb49a5cfe2a9f5174955ada350099f529
Jaime Mikael Hidalgo Nogales <jmhidalgo7@espe.edu.ec>
2025-10-31 15:03:20 -0500

    v0.1.0

---
+++ hw/hidalgo/u1/hw14SafeInJsonChickenFarmSimulator/ChickenFarmPython/Untitled-2.py
@@ -0,0 +1,16 @@
+[
+  {
+    "farmer_id": 1,
+    "name": "Mikael",
+    "coop_ids": [
+      1
+    ]
+  },
+  {
+    "farmer_id": 2,
+    "name": "Jason",
+    "coop_ids": [
+      22
+    ]
+  }
+]
