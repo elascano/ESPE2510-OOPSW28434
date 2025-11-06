5eb05751acd12f1308bc36905e52729333b7fceb
ANT0321 <adry.toap2034@gmail.com>
2025-11-06 17:52:24 -0500

    v0.1.2

---
+++ ws/toapanta/u1/ws04dependencies/ChikenFarmSimulator/src/ec/edu/espe/chikenfarmsimulator/view/ChikenFarmSimulator.java
@@ -0,0 +1,36 @@
+package ec.edu.espe.chikenfarmsimulator.view;
+
+import ec.edu.espe.chikenfarmsimulator.model.Chiken;
+//#include chikens.h si fuera en C
+
+/**
+ *
+ * @author ADRIAN TOAPANTA, Object Masters, @ESPE
+ */
+public class ChikenFarmSimulator {
+
+    public static void main(String args[]) {
+
+        System.out.println("This is my Chiken Farm Simulator");
+
+        Chiken chiken2 = new Chiken(0, "Maruja", "white", 1, true);
+
+        String owner;
+        int id = 1;
+        String name = "Lucy";
+        String color = "black and brown";
+        int age = 2;
+        boolean isMolting = false;
+
+        owner = "Kevin Chalan";
+
+        //Class  object
+        Chiken chiken;
+        chiken = new Chiken(id, name, color, age, isMolting);
+        System.out.println("The chiken is ---->" + chiken);
+
+        System.out.println("chiken id --->" + chiken.getId() + chiken.getName());
+        chiken.getId();
+
+    }
+}
