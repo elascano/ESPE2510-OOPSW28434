06c635f48024e8b1ebbf5bb137f7af96fcf8fc85
Jaime Mikael Hidalgo Nogales <jmhidalgo7@espe.edu.ec>
2025-11-06 20:00:55 -0500

    Delete ws/hidalgo/u1/

--- ws/hidalgo/u1/hw06Json/poop_model.py
+++
@@ -1,8 +0,0 @@
-class Poop:
-    def __init__(self, amount):
-        
-        self.amount = float(amount)
-        self.unit = "grams" 
-
-    def get_info(self):
-        return f"Amount of waste: {self.amount} {self.unit}."
