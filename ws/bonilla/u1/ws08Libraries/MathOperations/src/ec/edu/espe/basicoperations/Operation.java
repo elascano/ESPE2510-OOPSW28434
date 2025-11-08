3fded3e6d755dc3d71a0e1d3c35fb014e9863a00
Samiabc <asbonilla7@espe.edu.ec>
2025-11-08 11:24:10 -0500

    ws08Libraries v 0.1.0

---
+++ ws/bonilla/u1/ws08Libraries/MathOperations/src/ec/edu/espe/basicoperations/Operation.java
@@ -0,0 +1,23 @@
+package ec.edu.espe.basicoperations;
+
+/**
+ *
+ * @author Arelis Samantha Bonilla Cruz, Student, @ESPE
+ */
+public class Operation {
+    public static float add(float addend1, float addend2){
+        //T0D0  the very complex algorithm for addition
+        float sum;
+        sum = addend1 + addend2;
+        return sum;
+    }
+    
+    public static float subtract(float minuend, float subtrahend){
+        float difference;
+        //T0D0  the very complex algorithm
+        
+        difference = minuend - subtrahend;
+        return difference;
+    }
+    
+}
