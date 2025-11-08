54a046d634499261702d13160c739ff8562cad7e
Steven <ssloza1@espe.edu.ec>
2025-11-08 01:00:50 -0500

    ws08libraries v 0.0.2

---
+++ ws/loza/u1/ws08Libraries/MathOperation/src/ec/edu/espe/basicoperations/Operation.java
@@ -0,0 +1,24 @@
+package ec.edu.espe.basicoperations;
+
+/**
+ *
+ * @author Steven Loza 
+ */
+public class Operation {
+    public static float add(float addend1, float addend2){
+        //TODO the very complex algorith for addition
+        float sum;
+        sum= addend1 +addend2;
+        return sum;  
+    }
+   
+    public static float substract (float minuend, float subtrahend){
+        float difference;
+        //TODO the very complex algorithms
+        difference= minuend- subtrahend;
+        return difference;     
+    }
+    
+    
+    
+}
