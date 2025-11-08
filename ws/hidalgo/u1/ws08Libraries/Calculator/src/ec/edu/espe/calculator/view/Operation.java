fd9b01faf0d3ef713288732226e4226865131502
Jaime Mikael Hidalgo Nogales <jmhidalgo7@espe.edu.ec>
2025-11-08 11:19:45 -0500

    v1.0.1 ws08Libraries

---
+++ ws/hidalgo/u1/ws08Libraries/Calculator/src/ec/edu/espe/calculator/view/Operation.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ec.edu.espe.calculator.view;
+
+/**
+ *
+ * @author USUARIO
+ */
+class Operation {
+
+    static float substract(float minuend, float subtrahend) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    static float add(float addend1, float addend2) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
