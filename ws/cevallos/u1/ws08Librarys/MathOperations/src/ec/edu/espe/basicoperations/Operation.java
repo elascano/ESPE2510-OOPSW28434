e6e26048f91dfb0d9f39de2fae41d09f0449f33f
cevallosmateo07-byte <cevallosmateo07@gmail.com>
2025-11-08 00:45:51 -0500

    ws08 v0.1.1

---
+++ ws/cevallos/u1/ws08Librarys/MathOperations/src/ec/edu/espe/basicoperations/Operation.java
@@ -0,0 +1,20 @@
+package ec.edu.espe.basicoperations;
+/**
+ *
+ * @author Mateo Cevallos
+ */
+public class Operation {
+    public float add(float addend1, float addend2){
+        //TODO the very complex algorithm for addition
+        float sum;
+        sum = addend1 + addend2;
+        return sum;
+    }
+    public float substract(float minuend, float substrahend){
+        float difference;
+        difference = minuend - substrahend;
+        return difference;      
+    }
+    
+    
+}
