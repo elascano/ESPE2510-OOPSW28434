76c741e6c541243862cae5ce5a040354704b1d98
pablojosecollaguazonazate-svg <pablocollaguazonazate@gmail.com>
2025-11-07 23:32:31 -0500

    libraries

---
+++ ws/collaguazo/u1/ws08libraries/Nueva Carpeta/MathOperations/src/ec/edu/espe/basicoperations/Operation.java
@@ -0,0 +1,20 @@
+package ec.edu.espe.basicoperations;
+
+/**
+ *
+ * @author Pablo Collaguazo
+ */
+public class Operation {
+    public static float add(float addend1, float addend2){
+    //TODO the very complex algorithm for addtition
+    float sum;
+    sum = addend1 + addend2;
+    return sum;
+}
+    public static float subtract(float minuend, float subtrahend){
+        float difference;
+        //TODO very complex algorithm
+        difference = minuend - subtrahend;
+        return difference;
+    }
+}
