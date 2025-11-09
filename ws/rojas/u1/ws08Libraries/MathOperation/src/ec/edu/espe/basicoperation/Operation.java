3f86cac80751df9f6ffd78942e72fdc62105bba1
unknown <svp.jd.rojas@gmail.com>
2025-11-09 13:54:08 -0500

    v 0.1.0 ws08libraries

---
+++ ws/rojas/u1/ws08Libraries/MathOperation/src/ec/edu/espe/basicoperation/Operation.java
@@ -0,0 +1,25 @@
+package ec.edu.espe.basicoperation;
+
+/**
+ *
+ * @author Josue Rojas
+ */
+public class Operation {
+
+    public static float add(float addend1, float addend2) {
+        //TODO the very complex algorith for addition
+        float sum;
+        sum = addend1 + addend2;
+        return sum;
+    }
+
+    public static float subtract(float minuend, float subtrahend) {
+        float difference;
+
+        //TODO very complex algorith
+        difference = minuend - subtrahend;
+        return difference;
+    }
+
+}
+
