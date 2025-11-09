e867a1c3d059935ce1bf1e70ef31f5c2a88a61c0
bsgudino1416 <bsgudino@espe.edu.ec>
2025-11-09 14:05:02 -0500

    ws08lib v0.1.0

---
+++ ws/gudino/u1/ws08Libraries/MathOperation/src/ec/edu/espe/basicoperations/Operation.java
@@ -0,0 +1,23 @@
+package ec.edu.espe.basicoperations;
+
+/**
+ *
+ * @author Bryan Gudino, @ESPE
+ */
+public class Operation {
+    public static float add(float addend1, float addend2){
+        //TODO the very complex algorith for addition
+        float sum;
+        sum= addend1 +addend2;
+        return sum;  
+    }
+   
+    public static float substract (float minuend, float subtrahend){
+        float difference;
+        //TODO the very complex algorithms
+        difference= minuend- subtrahend;
+        return difference;     
+    }
+    
+    
+}
