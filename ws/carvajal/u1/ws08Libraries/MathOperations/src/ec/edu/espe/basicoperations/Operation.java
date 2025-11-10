39c934b79f856b518d13f63fe90ca52995168cf8
Josué Gabriel Carvajal Benites <josuecbgabriel@gmail.com>
2025-11-09 22:20:49 -0500

    v0.1.0

---
+++ ws/carvajal/u1/ws08Libraries/MathOperations/src/ec/edu/espe/basicoperations/Operation.java
@@ -0,0 +1,25 @@
+package ec.edu.espe.basicoperations;
+
+/**
+ *
+ * @author Josue Carvajal, THE ART OF PROGRAMMING, @ESPE
+ */
+public class Operation {
+    
+    public static float add(float addend1, float addend2){
+    
+        float sum;
+        sum = addend1 + addend2;
+        return sum;
+        
+    
+    }
+    
+    public static float substract(float minuend, float subtrahend){
+        
+        float difference;
+        difference = minuend - subtrahend;
+        return difference;
+    }
+    
+}
