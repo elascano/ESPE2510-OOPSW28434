95b1901b6d6ddc5912ffd6f72518ae7e4e4812cc
arelysotavalo <arelysotavalopjlt@gmail.com>
2025-11-13 16:16:39 -0500

    change names

---
+++ ws/otavalo/u1/ws05Dependencies/ChickenFarmSimulator/src/ec/edu/ec/chickenfarmsimulator/model/Chicken.java
@@ -0,0 +1,147 @@
+package ec.edu.ec.chickenfarmsimulator.model;
+
+/**
+ *
+ * @author Arelys Otavalo
+ * @version 0.1 
+ */
+public class Chicken {
+    private int id;
+    private String name;
+    private String color;
+    private int age;
+    private boolean isMolting;
+    
+    private void cluck(){
+        System.out.println(" chicken " + name + " is clucking cluck cluck cluck");
+    }
+
+    private void eat(){
+        System.out.println( " chicken " + name + " is eating, grains ");
+    }
+    
+    public void doStuff(){
+        cluck();
+        eat();
+        cluck();
+        poop(2);
+        poop(3);
+        eat ();
+        wander();
+        drink();
+        layAnEgg('M');
+        layAnEgg('L');
+    }
+    
+    public Poop poop(int amount){
+        Poop poop;
+        poop = new Poop(amount);
+        System.out.println(" chicken " + name + " is pooping " + poop);
+        return poop;
+    }
+    
+    /**
+     * Creates an egg of size size and returns to the calling function
+     * @param size Size of the eggs with possible values S, M, L
+     * @return and Egg of size size.
+     */
+    public Egg layAnEgg(char size){
+        Egg egg = new Egg(size);
+        System.out.println(" chicken " + name + " is laying " + egg.getSize() + " size egg");
+        return egg;
+    }
+    
+   public void drink(){
+       System.out.println(" chicken " + name + " is laying ");
+       
+   }
+   
+    public void wander(){
+        System.out.println(" chicken " + name + " is wandering ");
+        
+    }
+    @Override
+    public String toString() {  //ese to string es el q hace q el main se imprima
+        return "\nChicken{" + "\nid-->\t\t" + id + ", \nname--> \t" + name + ", \ncolor--> \t" + color + ", \nage-->\t\t" + age + ", \nisMolting -->" + isMolting + '}';
+    }
+    
+    public Chicken(int id, String name, String color, int age, boolean isMolting) {
+        this.id = id;
+        this.name = name;
+        this.color = color;
+        this.age = age;
+        this.isMolting = isMolting;
+    }
+
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return the color
+     */
+    public String getColor() {
+        return color;
+    }
+
+    /**
+     * @param color the color to set
+     */
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    /**
+     * @return the age
+     */
+    public int getAge() {
+        return age;
+    }
+
+    /**
+     * @param age the age to set
+     */
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    /**
+     * @return the isMolting
+     */
+    public boolean isIsMolting() {
+        return isMolting;
+    }
+
+    /**
+     * @param isMolting the isMolting to set
+     */
+    public void setIsMolting(boolean isMolting) {
+        this.isMolting = isMolting;
+    }
+    
+    
+}
