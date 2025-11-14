b967984caebdbaffa342840d9921d2f33e21cbce
cevallosmateo07-byte <cevallosmateo07@gmail.com>
2025-11-14 02:08:02 -0500

    renaming ws directories

---
+++ ws/cevallos/u1/ws05ChickeFarmSimulator/ChickenFarmSimulator/src/ec/edu/espe/chickenfarmsimulator/model/Egg.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ec.edu.espe.chickenfarmsimulator.model;
+
+/**
+ *
+ * @author Mateo Cevallos, Object Masters, @ESPE
+ */
+public class Egg {
+    private char size;
+
+    @Override
+    public String toString() {
+        return "Egg{" + "size=" + size + '}';
+    }
+    
+    
+
+    public Egg(char size) {
+        this.size = size;
+    }
+
+    /**
+     * @return the size
+     */
+    public char getSize() {
+        return size;
+    }
+
+    /**
+     * @param size the size to set
+     */
+    public void setSize(char size) {
+        this.size = size;
+    }
+    
+    
+}
