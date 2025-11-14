46f2f77f04dc9b1492b454bf6fc10994516be35a
Fernando Daniel Chicaiza Sambachi <xxfernan626xx@gmail.com>
2025-11-13 22:34:52 -0500

    hw v1.0

--- hw/medina/u1/hw08FileManagement/chickenFileManagementPy/model/Egg.py
+++
@@ -1,9 +0,0 @@
-class Egg:
-    def __init__(self, size: str):
-        self.size = size
-
-    def __str__(self):
-        return f"Egg{{ size = {self.size} }}"
-
-    def get_size(self):
-        return self.size
