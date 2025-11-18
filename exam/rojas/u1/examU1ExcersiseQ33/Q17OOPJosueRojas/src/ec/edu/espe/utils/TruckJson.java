f5da59c09ad186a81813fdc8fd151f56c8210355
unknown <svp.jd.rojas@gmail.com>
2025-11-18 17:38:47 -0500

    Exam v 0.1.0

---
+++ exam/rojas/u1/examU1ExcersiseQ33/Q17OOPJosueRojas/src/ec/edu/espe/utils/TruckJson.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ec.edu.espe.utils;
+
+/**
+ *
+ * @author Josue Rojas,Team Neon Cursed, @ESPE
+ */
+public class TruckJson {
+    
+}
---
+++ exam/rojas/u1/examU1ExcersiseQ33/Q17OOPJosueRojas/src/ec/edu/espe/view/TruckView.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ec.edu.espe.view;
+
+/**
+ *
+ * @author Josue Rojas,Team Neon Cursed, @ESPE
+ */
+public class TruckView {
+    
+}
