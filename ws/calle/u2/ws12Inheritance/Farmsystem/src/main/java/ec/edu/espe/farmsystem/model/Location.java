68f75471e4c2f403c9b9174cb7b321410af36077
emilyvillacis <emily.scarleth444@gmail.com>
2025-11-24 15:10:34 -0500

    ws12 Inheritance v 0.1.0

---
+++ ws/calle/u2/ws12Inheritance/Farmsystem/src/main/java/ec/edu/espe/farmsystem/model/Location.java
@@ -0,0 +1,51 @@
+package ec.edu.espe.farmsystem.model;
+
+/**
+ *
+ * @author Emily Calle, @ESPE
+ */
+public class Location {
+    private int xCoordinate;
+    private int yCoordinate;
+
+    @Override
+    public String toString() {
+        return "Location{" + "xCoordinate=" + xCoordinate + ", yCoordinate=" + yCoordinate + '}';
+    }
+
+    
+    public Location(int xCoordinate, int yCoordinate) {
+        this.xCoordinate = xCoordinate;
+        this.yCoordinate = yCoordinate;
+    }
+
+    
+    /**
+     * @return the xCoordinate
+     */
+    public int getxCoordinate() {
+        return xCoordinate;
+    }
+
+    /**
+     * @param xCoordinate the xCoordinate to set
+     */
+    public void setxCoordinate(int xCoordinate) {
+        this.xCoordinate = xCoordinate;
+    }
+
+    /**
+     * @return the yCoordinate
+     */
+    public int getyCoordinate() {
+        return yCoordinate;
+    }
+
+    /**
+     * @param yCoordinate the yCoordinate to set
+     */
+    public void setyCoordinate(int yCoordinate) {
+        this.yCoordinate = yCoordinate;
+    }
+
+}
