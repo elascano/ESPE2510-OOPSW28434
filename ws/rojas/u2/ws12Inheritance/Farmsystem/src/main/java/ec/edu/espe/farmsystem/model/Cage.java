f425f7ab364f3224252f74bcf97945c3927fae7d
NuevoUsuario <correo@nuevo.com>
2025-11-24 15:26:40 -0500

    ws12Inheritance v 0.1.0

---
+++ ws/rojas/u2/ws12Inheritance/Farmsystem/src/main/java/ec/edu/espe/farmsystem/model/Cage.java
@@ -0,0 +1,81 @@
+package ec.edu.espe.farmsystem.model;
+/**
+ *
+ * @author Josue Rojas in Espe
+ */
+public class Cage {
+    private int id;
+    private String description;
+    private int type;//1 coop, 2 stable, 3 pens
+    private Location location;
+
+    @Override
+    public String toString() {
+        return "Cage{" + "id=" + id + ", description=" + description + ", type=" + type + ", location=" + location + '}';
+    }
+
+    public Cage(int id, String description, int type, Location location) {
+        this.id = id;
+        this.description = description;
+        this.type = type;
+        this.location = location;
+    }
+
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the description
+     */
+    public String getDescription() {
+        return description;
+    }
+
+    /**
+     * @param description the description to set
+     */
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    /**
+     * @return the type
+     */
+    public int getType() {
+        return type;
+    }
+
+    /**
+     * @param type the type to set
+     */
+    public void setType(int type) {
+        this.type = type;
+    }
+
+    /**
+     * @return the location
+     */
+    public Location getLocation() {
+        return location;
+    }
+
+    /**
+     * @param location the location to set
+     */
+    public void setLocation(Location location) {
+        this.location = location;
+    }
+
+    
+}
