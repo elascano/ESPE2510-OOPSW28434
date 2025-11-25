6f47c5af8bb32f8fb998171ecf1c75215e6146fa
arelysotavalo <arelysotavalopjlt@gmail.com>
2025-11-24 23:21:34 -0500

    inheritance v 0.1.0

---
+++ ws/otavalo/u2/ws13TheoricalExercise/TheoreticalExercise/src/main/java/ec/edu/espe/theoreticalexercise/model/A.java
@@ -0,0 +1,10 @@
+package ec.edu.espe.theoreticalexercise.model;
+
+/**
+ *
+ * @author Arelys Otavalo
+ */
+public interface A {
+    public void m(int a); //funcion abstracta pq no hace nd mas q la declaración
+
+}
