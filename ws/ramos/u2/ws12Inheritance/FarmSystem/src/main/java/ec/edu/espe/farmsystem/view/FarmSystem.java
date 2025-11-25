53741f126587c424072de646404151c20ee58a31
PauloRamos24 <paulo.alramos24@gmail.com>
2025-11-24 23:30:38 -0500

    Inherence v0.1.0

---
+++ ws/ramos/u2/ws12Inheritance/FarmSystem/src/main/java/ec/edu/espe/farmsystem/view/FarmSystem.java
@@ -0,0 +1,47 @@
+package ec.edu.espe.farmsystem.view;
+
+import ec.edu.espe.farmsystem.model.Cage;
+import ec.edu.espe.farmsystem.model.FarmAnimal;
+import ec.edu.espe.farmsystem.model.Location;
+import java.util.Date;
+
+/**
+ *
+ * @author Paulo Ramos
+ */
+public class FarmSystem {
+    public static void main(String[] args) {
+      
+        int id;
+        String  breed;
+        Date bornOn;
+        String gender;
+        boolean isAbleToReproduce;
+        float weight;
+        Cage cage;
+        Location location;
+        int xCoordinate;
+        int yCoordinate;
+    
+        xCoordinate = 10;
+        yCoordinate = 20;
+        weight=10.4F;
+        isAbleToReproduce= true;
+        gender="male";
+        
+        bornOn=new Date(2025,2,1);
+        breed = "Holstein";       
+    
+        location= new Location(xCoordinate,yCoordinate); 
+        cage= new Cage(1,"stable for cows",2,location);
+    
+        FarmAnimal farmAnimal = new FarmAnimal(1,breed,bornOn,gender,isAbleToReproduce,weight,cage);
+        System.out.println("FarmAnimal -->"+ farmAnimal);
+        
+    }
+    
+    
+
+    
+    
+}
