f647edf64b9974df97a581c992087fa79f9f8a43
Joseph-Benjamin-Medina-Sanchez <joseph3elcrac@gmail.com>
2025-11-24 23:01:42 -0500

    ws13TheoricalExercise v 0.1.0

---
+++ ws/medina/u2/ws13TheoricalExcercise/TheoreticalExercise/src/main/java/ec/edu/espe/theoreticalexercise/model/B.java
@@ -0,0 +1,43 @@
+
+package ec.edu.espe.theoreticalexercise.model;
+
+/**
+ *
+ * @author Joseph B. Medina
+ */
+public abstract class B implements A {
+    private int a;
+    private int b;
+
+    @Override
+    public String toString() {
+        return "B{" + "a=" + a + ", b=" + b + '}';
+    }
+        
+
+    public B(int a, int b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    public int getA() {
+        return a;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+
+    public int getB() {
+        return b;
+    }
+
+    public void setB(int b) {
+        this.b = b;
+    }
+    
+    
+    
+    
+    
+}
