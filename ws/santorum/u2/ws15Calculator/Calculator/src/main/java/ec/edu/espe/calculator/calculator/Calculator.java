82bfc84f2f92cd51651f4ffa8b43fb637bca6a20
salemyst <tysantorum@espe.edu.ec>
2025-11-27 13:12:10 -0500

    Calculator Tests

---
+++ ws/santorum/u2/ws15Calculator/Calculator/src/main/java/ec/edu/espe/calculator/calculator/Calculator.java
@@ -0,0 +1,13 @@
+
+package ec.edu.espe.calculator.calculator;
+
+/**
+ *
+ * @author LABS-ESPE
+ */
+public class Calculator {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
