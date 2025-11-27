fb9c107fb6aee9970590eca9df57b9535198278b
emilyvillacis <emily.scarleth444@gmail.com>
2025-11-27 13:21:52 -0500

    ws15 v 0.1.0

---
+++ ws/calle/u2/ws15TestCases/Calculator/src/main/java/utils/BasicOperation.java
@@ -0,0 +1,14 @@
+package utils;
+
+/**
+ *
+ * @author Emily Calle, @ESPE
+ */
+public class BasicOperation {
+
+    public float add(float addend1, float addend2){
+        float sum;
+        sum = addend1 + addend2;
+        return sum;
+    }
+}
