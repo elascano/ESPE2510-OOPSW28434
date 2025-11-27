2fe9d608abe220c384769ebfcc4ba5723f7d1719
Joseph-Benjamin-Medina-Sanchez <joseph3elcrac@gmail.com>
2025-11-27 13:18:06 -0500

    hw15UnitTest v 0.1.0

---
+++ ws/medina/u2/hw15UnitTest/src/main/java/utils/basicOperation.java
@@ -0,0 +1,15 @@
+package utils;
+
+/**
+ *
+ * @author Joseph Medina Espe
+ */
+public class basicOperation {
+    
+    public float add( float addend1, float addend2){
+        float sum;
+        sum = addend1 + addend2;
+        return sum;
+    }
+    
+}
