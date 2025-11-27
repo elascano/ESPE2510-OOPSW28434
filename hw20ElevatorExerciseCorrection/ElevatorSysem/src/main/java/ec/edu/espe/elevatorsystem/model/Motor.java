e970b57a3d431640a0e73830b6da4f484b9be2da
Jaime Mikael Hidalgo Nogales <jmhidalgo7@espe.edu.ec>
2025-11-27 18:38:27 -0500

    v0.1.0 Elevator Exercise Correction

---
+++ hw20ElevatorExerciseCorrection/ElevatorSysem/src/main/java/ec/edu/espe/elevatorsystem/model/Motor.java
@@ -0,0 +1,12 @@
+package ec.edu.espe.elevatorsystem.model;
+
+public class Motor {
+
+    public void startMoving() {
+        System.out.println("[Motor] Iniciando movimiento...");
+    }
+
+    public void stopMoving() {
+        System.out.println("[Motor] Deteniendo movimiento...");
+    }
+}
