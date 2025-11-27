96c6a43e5e47da42a1a05bc245daddfa08638d8e
ANT0321 <adry.toap2034@gmail.com>
2025-11-27 15:36:14 -0500

    v 0.1.0

---
+++ ws/toapanta/u2/ws15UnitTest/ws15UnitTest/Calculator/src/main/java/ec/edu/espe/calculator/calculator/Calculator.java
@@ -0,0 +1,13 @@
+
+package ec.edu.espe.calculator.calculator;
+
+/**
+ *
+ * @author Adrian Toapanta, Student OOP, @ESPE
+ */
+public class Calculator {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
