5b39a98d27029408a29d4a9ce7231a50ba856f3d
Joseph-Benjamin-Medina-Sanchez <joseph3elcrac@gmail.com>
2025-11-26 23:37:16 -0500

    hw20 CorrectionElevatorExample v 0.2.0

---
+++ hw/medina/u2/hw20ElevatorExerciseCorrection/ec.edu.espe_ElevatorSystem_jar_1.0-SNAPSHOT/src/main/java/ec/edu/espe/elevatorsystem/model/IAlarm.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ec.edu.espe.elevatorsystem.model;
+
+/**
+ *
+ * @author Steven Loza @ESPE
+ */
+public interface IAlarm {
+    void triggerAlarm();
+}
+
