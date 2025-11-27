7a8ffaec9f9883f942a1ef06cc5f30ade3d40113
444jdrojas <svp.jd.rojas@gmail.com>
2025-11-27 09:59:47 -0500

    Elevator System

---
+++ hw/rojas/u2/hw20ElevatorExerciseCorrection/ElevatorSystem/src/main/java/ec/edu/espe/ElevatorSystem/model/Employee.java
@@ -0,0 +1,9 @@
+package ec.edu.espe.ElevatorSystem.model;
+
+/**
+ * @author JOSUE ROJAS y THAIS SANTORUM
+ */
+
+public class Employee extends Person {
+    public Employee(float weight) { super(weight); }
+}
