81a9b5425a958fbd9a0c0522ae37229a29973056
Maryuri3 <maryurisalome3@gmail.com>
2025-11-27 20:09:45 -0500

    ws15 v 0.1.0

---
+++ ws/quina/u2/ws15UnitTest/Calculator/src/main/java/utils/basicOperation.java
@@ -0,0 +1,14 @@
+package utils;
+
+/**
+ *
+ * @author Maryuri Quina, @ESPE
+ */
+public class basicOperation {
+    
+    public float add(float addend1, float addend2){
+        float sum;
+        sum = addend1+addend2;
+        return sum;
+    }
+}
