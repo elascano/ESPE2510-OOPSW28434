721c87717a74845afe93f60776304a5f18fa26cc
unknown <svp.jd.rojas@gmail.com>
2025-11-27 23:30:00 -0500

    ws15TestCases v 0.1.0

---
+++ ws/rojas/u2/ws15TestCases/Calculator/src/main/java/utils/basicOperation.java
@@ -0,0 +1,13 @@
+package utils;
+
+/**
+ *
+ * @author LABS-ESPE
+ */
+public class basicOperation {
+    public float add(float addend1,float addend2){
+    float sum;
+    sum = addend1+addend2;
+    return sum;
+    }
+}
