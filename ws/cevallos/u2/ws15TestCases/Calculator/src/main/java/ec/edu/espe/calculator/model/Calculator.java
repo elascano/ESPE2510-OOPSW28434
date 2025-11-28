b2f22bc4c2bc10551aa5fd5fb4dcaa2e8e697dc4
cevallosmateo07-byte <cevallosmateo07@gmail.com>
2025-11-28 08:45:17 -0500

    ws15 v0.1.0

---
+++ ws/cevallos/u2/ws15TestCases/Calculator/src/main/java/ec/edu/espe/calculator/model/Calculator.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ec.edu.espe.calculator.model;
+
+/**
+ *
+ * @author Mateo Cevallos Object Masters
+ */
+public class Calculator {
+    
+}
