f06062178b273d86df827f00ff5b7812f818280c
arelysotavalo <arelysotavalopjlt@gmail.com>
2025-12-02 23:17:29 -0500

    hw 23 v 0.1.0

---
+++ hw/otavalo/u2/hw23GUIs/Product/src/main/java/ec/edu/espe/product/model/Product.java
@@ -0,0 +1,18 @@
+package ec.edu.espe.product.model;
+
+/**
+ *
+ *  @author @author Arelys Otavalo, The POOwer Rangers Of Programming
+ */
+public class Product {
+    int id;
+    int barCode;
+    String description;
+    String supplier;
+    int quantity;
+    String productCode;
+    float price;
+    String date;
+    String type; //Brakes, Shock Absorbes, Brake Shoes, Other
+   
+}
