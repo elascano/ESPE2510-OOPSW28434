c1d2ca7a54e4a57b6604be733c2c8b5d670195c1
arelysotavalo <arelysotavalopjlt@gmail.com>
2025-12-09 00:59:03 -0500

    delete wrong file

--- hw/otavalo/u2/hw25MongoDBConnections/contacts-app/renderer.js
+++
@@ -1,8 +0,0 @@
-const { contextBridge, ipcRenderer } = require("electron");
-
-contextBridge.exposeInMainWorld("api", {
-    saveContact: (data) => ipcRenderer.invoke("save-contact", data),
-    findContact: (query) => ipcRenderer.invoke("find-contact", query),
-    // ELIMINADO: deleteContact
-    updateContact: (id, data) => ipcRenderer.invoke("update-contact", id, data) 
-});
