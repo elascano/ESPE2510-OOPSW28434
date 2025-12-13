905b019545e7b7aba02a5cc335bfc4cff7dd823d
emilyvillacis <emily.scarleth444@gmail.com>
2025-12-13 16:08:25 -0500

    ws21Navegation

---
+++ ws/calle/u2/ws21Navegation/ContactsBook/src/main/java/ec/edu/espe/contactsbook/model/User.java
@@ -0,0 +1,61 @@
+package ec.edu.espe.contactsbook.model;
+/**
+ * 
+ * @author Emily Calle, @ESPE
+ */
+
+public class User{
+    private String userName;
+    private String password;
+    private String type;
+
+    public User(String user, String password, String type) {
+        this.userName = user;
+        this.password = password;
+        this.type = type;
+    }
+    
+    
+    /**
+     * @return the user
+     */
+    public String getUserName() {
+        return userName;
+    }
+
+    /**
+     * @param user the user to set
+     */
+    public void setUserName(String user) {
+        this.userName = user;
+    }
+
+    /**
+     * @return the password
+     */
+    public String getPassword() {
+        return password;
+    }
+
+    /**
+     * @param password the password to set
+     */
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    /**
+     * @return the type
+     */
+    public String getType() {
+        return type;
+    }
+
+    /**
+     * @param type the type to set
+     */
+    public void setType(String type) {
+        this.type = type;
+    }
+    
+}
