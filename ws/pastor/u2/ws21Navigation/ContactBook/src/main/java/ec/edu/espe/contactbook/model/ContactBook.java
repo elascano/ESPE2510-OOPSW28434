c058857baba4eed9d81b84715dcd0d5fcef94600
mthws-hub <mspastor.h@gmail.com>
2025-12-13 21:24:30 -0500

    ws19/ws20/ws21 v 0.1.0

---
+++ ws/pastor/u2/ws21Navigation/ContactBook/src/main/java/ec/edu/espe/contactbook/model/ContactBook.java
@@ -0,0 +1,14 @@
+
+
+package ec.edu.espe.contactbook.model;
+
+/**
+ *
+ * @author Mathews Pastor, POOwer Ranger of Programing, @ESPE
+ */
+public class ContactBook {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
