f7d7d69c60350a1e882dcd3652ea85d9676b5f3d
Adrian Michael Toapanta Orejuela <adry.toap2034@gmail.com>
2025-12-16 13:45:29 -0500

    Add files via upload
    "v 0.1.0"

---
+++ ws/toapanta/u2/ws23Evaluationsolution/Q31_51/src/main/java/ec/edu/espe/q31_51/model/B.java
@@ -0,0 +1,40 @@
+
+package ec.edu.espe.q31_51.model;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Arelis Samantha Bonilla Cruz, @ESPE
+ */
+public class B extends A{
+private ArrayList<H> hs ;
+
+    @Override
+    public String toString() {
+        return "B{" + "hs=" + hs + super.toString()+'}';
+    }
+
+
+
+    public B(ArrayList<H> hs, ArrayList<A> as) {
+        super(as);
+        this.hs = hs;
+    }
+  
+
+    /**
+     * @return the hs
+     */
+    public ArrayList<H> getHs() {
+        return hs;
+    }
+
+    /**
+     * @param hs the hs to set
+     */
+    public void setHs(ArrayList<H> hs) {
+        this.hs = hs;
+    }
+
+}
---
+++ ws/toapanta/u2/ws23Evaluationsolution/Q31_51/src/main/java/ec/edu/espe/q31_51/model/F.java
@@ -0,0 +1,16 @@
+
+package ec.edu.espe.q31_51.model;
+
+/**
+ *
+ * @author Arelis Samantha Bonilla Cruz, @ESPE
+ */
+public class F {
+
+    @Override
+    public String toString() {
+        return "F{" + '}';
+    }
+
+  
+}
