f8419eaf0767cbc869ccce9331e70ee7a55ff990
JOSUE GABRIEL CARVAJAL BENITEZ <josuecbgabriel@gmail.com>
2025-12-17 11:33:01 -0500

    v0.1.0

---
+++ exam/carvajal/u2/Q38/src/ec/ede/espe/Q38/model/B.java
@@ -0,0 +1,11 @@
+package ec.ede.espe.Q38.model;
+
+/**
+ *
+ * @author Josue Carvajal,The Art of Programming, @ESPE
+ */
+public class B {
+    public void r(){
+    
+    }
+}
---
+++ exam/carvajal/u2/Q38/src/ec/ede/espe/Q38/model/G.java
@@ -0,0 +1,24 @@
+package ec.ede.espe.Q38.model;
+
+/**
+ *
+ * @author Josue Carvajal,The Art of Programming, @ESPE
+ */
+public class G {
+    private J j;
+
+    /**
+     * @return the j
+     */
+    public J getJ() {
+        return j;
+    }
+
+    /**
+     * @param j the j to set
+     */
+    public void setJ(J j) {
+        this.j = j;
+    }
+    
+}
