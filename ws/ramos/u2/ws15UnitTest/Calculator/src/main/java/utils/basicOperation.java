cf0937e38e21e4de5b0e7baff6276ac973220069
PauloRamos24 <paulo.alramos24@gmail.com>
2025-12-17 17:40:36 -0500

    ws15 v0.1.0

---
+++ ws/ramos/u2/ws15UnitTest/Calculator/src/main/java/utils/basicOperation.java
@@ -0,0 +1,14 @@
+package utils;
+
+/**
+ *
+ * @author Paulo Ramos
+ */
+public class basicOperation {
+    
+    public float add(float addend1, float addend2){
+        float sum;
+        sum = addend1+addend2;
+        return sum;
+    }
+}
---
+++ ws/ramos/u2/ws15UnitTest/Calculator/src/test/java/utils/basicOperationTest.java
@@ -0,0 +1,61 @@
+package utils;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+
+/**
+ *
+ * @author Paulo Ramos
+ */
+public class basicOperationTest {
+    
+    public basicOperationTest() {
+    }
+
+    @BeforeAll
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterAll
+    public static void tearDownClass() throws Exception {
+    }
+
+    @BeforeEach
+    public void setUp() throws Exception {
+    }
+
+    @AfterEach
+    public void tearDown() throws Exception {
+    }
+
+    @org.junit.jupiter.api.Test
+    public void testSomeMethod() {
+        fail("The test case is a prototype.");
+    }
+
+    @Test
+    public void testAdd() {
+        System.out.println("add");
+        float addend1 = 1.2F;
+        float addend2 = 2.4F;
+        basicOperation instance = new basicOperation();
+        float expResult = 3.6F;
+        float result = instance.add(addend1, addend2);
+        assertEquals(expResult, result, 0);
+    }
+    
+        @Test
+    public void testAddNegatives() {
+        System.out.println("add");
+        float addend1 = -1.0F;
+        float addend2 = -2.0F;
+        basicOperation instance = new basicOperation();
+        float expResult = -3.0F;
+        float result = instance.add(addend1, addend2);
+        assertEquals(expResult, result, 0);
+    }
+}
