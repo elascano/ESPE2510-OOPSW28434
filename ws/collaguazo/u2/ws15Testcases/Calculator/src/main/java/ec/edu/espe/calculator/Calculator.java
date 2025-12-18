1ca4aecaf099e209ef3469374093b2eeeb52d406
pablojosecollaguazonazate-svg <pablocollaguazonazate@gmail.com>
2025-12-18 00:21:36 -0500

    ws

---
+++ ws/collaguazo/u2/ws15Testcases/Calculator/src/main/java/ec/edu/espe/calculator/Calculator.java
@@ -0,0 +1,13 @@
+
+package ec.edu.espe.calculator;
+
+/**
+ *
+ * @author Pablo Collaguazo
+ */
+public class Calculator {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
