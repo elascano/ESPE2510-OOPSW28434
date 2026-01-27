d776512aec9dfd0ff3a6637865c1c8f4d62a3f6f
444jdrojas <svp.jd.rojas@gmail.com>
2026-01-27 00:50:33 -0500

    hw33-34

---
+++ hw/rojas/u3/hw33Observer/Observer/src/main/java/ec/edu/espe/observer/model/IBM.java
@@ -0,0 +1,31 @@
+package ec.edu.espe.observer.model;
+
+/**
+ *
+ * @author JOSUE
+ */
+public class IBM extends Stock {
+    
+    public IBM(String symbol, double price) {
+        this.symbol = symbol;
+        this.price = price;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+        notifyObservers(price);
+    }
+
+    public String getSymbol() {
+        return symbol;
+    }
+
+    public void setSymbol(String symbol) {
+        this.symbol = symbol;
+        notifyObservers(symbol);
+    }
+}
---
+++ hw/rojas/u3/hw34Strategy/Strategy/src/main/java/ec/edu/espe/strategy/model/InsertionSort.java
@@ -0,0 +1,13 @@
+package ec.edu.espe.strategy.model;
+
+/**
+ *
+ * @author JOSUE
+ */
+public class InsertionSort implements SortingStrategy {
+    
+    public int[] sort(int data[]) {
+        System.out.println("Sorting using InsertionSort");
+        return data;
+    }
+}
