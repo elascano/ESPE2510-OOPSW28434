aeee96b396a379cc7977b555648fd301d6ed4ea3
Maryuri3 <maryurisalome3@gmail.com>
2026-01-26 22:44:51 -0500

    hw33 v0.1.0

---
+++ hw/quina/u3/hw34Strategy/Strategy/src/main/java/ec/edu/espe/strategy/model/QuickSort.java
@@ -0,0 +1,13 @@
+package ec.edu.espe.strategy.model;
+
+/**
+ *
+ * @author Maryuri Quiña, @ESPE
+ */
+public class QuickSort implements SortingStrategy {
+
+    public int[] sort(int data[]) {
+        System.out.println("Sorting using QuickSort");
+        return data;
+    }
+}
