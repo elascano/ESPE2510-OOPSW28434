f7bf71e5989936c707f0832e17b2bcaa4264830d
arelysotavalo <arelysotavalopjlt@gmail.com>
2026-01-26 20:11:34 -0500

    hw33 ObserverCode

---
+++ hw/otavalo/u3/hw33ObserverCode/Observer/src/main/java/ec/edu/espe/observer/model/IInvestor.java
@@ -0,0 +1,13 @@
+package ec.edu.espe.observer.model;
+
+/**
+ *
+ * @author Arelys Otavalo, the POOwer Rangers of Programming
+ */
+public interface IInvestor {
+
+    void update(Stock stock, Object args);
+
+}
+
+
