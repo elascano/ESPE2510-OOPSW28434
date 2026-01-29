648f42262c003cbccb342fd45124072d2cc0da06
Samiabc <asbonilla7@espe.edu.ec>
2026-01-29 00:56:37 -0500

    hw33 v 0.1.0

---
+++ hw/bonilla/u3/hw33ObserverCode/Observer/src/main/java/ec/edu/espe/observer/model/IInvestor.java
@@ -0,0 +1,9 @@
+package ec.edu.espe.observer.model;
+
+/**
+ *
+ * @author Arelis Samantha Bonilla Cruz, Student, @ESPE
+ */
+public interface IInvestor {
+    void update(Stock stock, Object args);
+}
---
+++ hw/bonilla/u3/hw33ObserverCode/Observer/src/main/java/ec/edu/espe/observer/model/Investor.java
@@ -0,0 +1,23 @@
+package ec.edu.espe.observer.model;
+
+/**
+ *
+ * @author Arelis Samantha Bonilla Cruz, Student, @ESPE
+ */
+public class Investor implements IInvestor {
+
+    private String name;
+
+    public Investor(String name) {
+        this.name = name;
+    }
+
+    public void update(Stock stock, Object args) {
+        System.out.println("Notified observer " + name);
+        if (args instanceof String) {
+            System.out.println("The symbol of " + stock.getSymbol() + " changed to: " + args);
+        } else if (args instanceof Double) {
+            System.out.println("The price of " + stock.getSymbol() + " changed to: " + args);
+        }
+    }
+}
