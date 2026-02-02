8e577679cd3c2b8b523602d638d85bb726e93c9e
kevanthoc249 <kachalan@espe.edu.ec>
2026-02-02 09:45:07 -0500

    hw Strategy

---
+++ hw/chalan/u3/hw34ObserverCode/Observer/src/main/java/ec/edu/espe/observer/model/IInvestor.java
@@ -0,0 +1,13 @@
+package ec.edu.espe.observer.model;
+
+/**
+ *
+ * @author Kevin Chalan, OBJECT MASTER, OOP
+ */
+public interface IInvestor {
+
+    void update(Stock stock, Object args);
+
+}
+
+
