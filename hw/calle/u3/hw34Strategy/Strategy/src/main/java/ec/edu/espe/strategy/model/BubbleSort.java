1ed235cde01dec13b1a75b530575deb69f97a01c
emilyvillacis <emily.scarleth444@gmail.com>
2026-02-01 21:40:16 -0500

    hw33,34

---
+++ hw/calle/u3/hw34Strategy/Strategy/src/main/java/ec/edu/espe/strategy/model/BubbleSort.java
@@ -0,0 +1,13 @@
+package ec.edu.espe.strategy.model;
+
+/**
+ *
+ * @author Emily Calle, @ESPE
+ */
+public class BubbleSort implements SortingStrategy {
+    public int[] sort(int data[]) {
+        System.out.println("Sorting using BubbleSort");
+        // Lógica del algoritmo aquí
+        return data;
+    }
+}
---
+++ hw/calle/u3/hw34Strategy/Strategy/src/main/java/ec/edu/espe/strategy/model/QuickSort.java
@@ -0,0 +1,13 @@
+package ec.edu.espe.strategy.model;
+
+/**
+ *
+ * @author Emily Calle, @ESPE
+ */
+public class QuickSort implements SortingStrategy {
+    public int[] sort(int data[]) {
+        System.out.println("Sorting using QuickSort");
+        // Lógica del algoritmo aquí
+        return data;
+    }
+}
