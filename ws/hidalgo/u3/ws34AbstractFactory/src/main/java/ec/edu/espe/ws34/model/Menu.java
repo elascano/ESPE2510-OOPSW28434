8fd3b673dd07248994f4ab7cccc70ec8be63c49c
Jaime Mikael Hidalgo Nogales <jmhidalgo7@espe.edu.ec>
2026-02-02 21:50:53 -0500

    ws34 v0.1.0

---
+++ ws/hidalgo/u3/ws34AbstractFactory/src/main/java/ec/edu/espe/ws34/model/Menu.java
@@ -0,0 +1,11 @@
+package ec.edu.espe.ws34.model;
+
+/**
+ *
+ * @author Mikael Hidalgo
+ */
+public abstract class Menu {
+    public String caption;
+    public abstract void paint();
+
+}
---
+++ ws/hidalgo/u3/ws34AbstractFactory/src/main/java/ec/edu/espe/ws34/model/WinButton.java
@@ -0,0 +1,12 @@
+
+package ec.edu.espe.ws34.model;
+
+/**
+ *
+ * @author Mikael Hidalgo
+ */
+public class WinButton  extends Button{
+    public void paint(){
+        System.out.println(" I am WinButton" + caption);
+    }
+}
