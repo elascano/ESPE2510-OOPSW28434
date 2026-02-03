1c23e3439a7a7447d5faf286f3898637c9da36b8
MattOso182 <mateo.oso182@gmail.com>
2026-02-02 22:20:36 -0500

    hw

---
+++ hw/aymacana/u3/hw34Observer/Observer/src/main/java/ec/edu/espe/observer/model/IInvestor.java
@@ -0,0 +1,10 @@
+package ec.edu.espe.observer.model;
+
+/**
+ *
+ * @author Mateo Aymacaña, T.A.P. The Art Of Programming
+ */
+public interface IInvestor {
+
+    void update(Stock stock, Object args);
+}
