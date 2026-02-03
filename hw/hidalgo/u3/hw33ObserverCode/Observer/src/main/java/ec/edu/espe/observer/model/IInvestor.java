025d8f4e718a3d5ddcbfdfc2a2c63161dd9ebe3f
Jaime Mikael Hidalgo Nogales <jmhidalgo7@espe.edu.ec>
2026-02-02 20:03:27 -0500

    hw33 v0.1.0

---
+++ hw/hidalgo/u3/hw33ObserverCode/Observer/src/main/java/ec/edu/espe/observer/model/IInvestor.java
@@ -0,0 +1,13 @@
+package ec.edu.espe.observer.model;
+
+/**
+ *
+ * @author Mikael Hidalgo, the POOwer Rangers of Programming
+ */
+public interface IInvestor {
+
+    void update(Stock stock, Object args);
+
+}
+
+
